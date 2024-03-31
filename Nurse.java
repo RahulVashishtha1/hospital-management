f685a0edd659412e5f7d172b2269ad7fa19bfe01
Rahul <68159064+RahulVashishtha1@users.noreply.github.com>
2024-03-31 13:19:06 +0530

    Built getters and setters for Employee, Doctor and Nurse class

--- Nurse.java
+++ Nurse.java
@@ -1,3 +1,12 @@
 public class Nurse {
-    
+
+    public int numberOfPatientsAssigned = 0;
+
+    void setNumberOfPatientsAssigned() {
+        numberOfPatientsAssigned++;
+    }
+
+    public int getNumberOfPatientsAssigned() {
+        return numberOfPatientsAssigned;
+    }
 }
